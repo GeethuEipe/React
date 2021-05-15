2f9774d190d0a1b5837bd5ec06ac76cd6751b774
GeethuEipe <78561027+GeethuEipe@users.noreply.github.com>
2021-05-15 05:54:03 +0200

    Update ImageView.js

--- src/ImageView/ImageView.js
+++ src/ImageView/ImageView.js
@@ -13,12 +13,7 @@ const ImageView = (props) => {
         <div className="col-sm-12">
           <div className="imageView__container">
             <img src={image} alt={tags} className="imageView__img img-responsive" />
-            <div className="imageView__copyright">
-              <p>&copy;pixabay</p>
-            </div>
             <div className="imageView__text">
-              <h4>Credit: <span>{owner}</span></h4>
-              <h4>Download: <span><a target="_blank" href={pageURL }>GO TO DOWNLOAD</a></span></h4>
               <button className="active-recipe__button">
                 <Link to="/">Home</Link>
               </button>
